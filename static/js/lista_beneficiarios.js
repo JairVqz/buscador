c1f88215f72a23cb33744684d37ea1489365e70e
jairVqz <jair10.vasquez11@gmail.com>
2024-10-30 16:02:43 -0600

    Fix mostrar/ocultar filtros

--- static/js/lista_beneficiarios.js
+++ static/js/lista_beneficiarios.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const filtrosBusquedaAvanzada = document.getElementById("filtrosBusquedaAvanzada");
     const btnMostrarFiltros = document.getElementById("consultaavan");
-    const btnBuscar = document.getElementById("btnBuscar");
-    const busquedaForm = document.getElementById("busquedaForm");
 
     function ocultarMostrarBusqueda() {
         if (filtrosBusquedaAvanzada.style.display === 'none' || filtrosBusquedaAvanzada.style.display === '') {
