f9e8869358026cb897b06eaf3a0c58fbd55d3604
jairVqz <jair10.vasquez11@gmail.com>
2024-10-30 12:14:28 -0600

    ajustando a solo buscar

--- beneficiario/urls.py
+++ beneficiario/urls.py
@@ -4,11 +4,5 @@ from django.contrib import admin
 
 urlpatterns=[
     path('admin/', admin.site.urls),
-    path('beneficiario/', views.beneficiariosApi),
-    path('beneficiario/<int:id>/', views.beneficiariosApi, name='beneficiario-detail'),
     path('', views.lista_beneficiarios, name='lista_beneficiarios'),
-    path('formulario/', views.formulario_beneficiario, name='formulario_beneficiario'),
-    path('agregar_beneficiario/', views.agregar_beneficiario, name='agregar_beneficiario'),
-    path('eliminar_beneficiario/<int:id>/', views.eliminar_beneficiario, name='eliminar_beneficiario'),
-    path('editar_beneficiario/<int:id>/', views.editar_beneficiario, name='editar_beneficiario'),
 ]
--- static/js/lista_beneficiarios.js
+++ static/js/lista_beneficiarios.js
@@ -1,12 +1,17 @@
-const filtrosBusquedaAvanzada = document.getElementById("filtrosBusquedaAvanzada");
-const btnMostrarFiltros = document.getElementById("consultaavan");
+document.addEventListener("DOMContentLoaded", function() {
+    const filtrosBusquedaAvanzada = document.getElementById("filtrosBusquedaAvanzada");
+    const btnMostrarFiltros = document.getElementById("consultaavan");
+    const btnBuscar = document.getElementById("btnBuscar");
+    const busquedaForm = document.getElementById("busquedaForm");
 
-function ocultarMostrarBusqueda() {
-    if (filtrosBusquedaAvanzada.style.display === 'none' || filtrosBusquedaAvanzada.style.display === '') {
-        filtrosBusquedaAvanzada.style.display = 'block'; // Muestra el elemento
-    } else {
-        filtrosBusquedaAvanzada.style.display = 'none'; // Oculta el elemento
+    function ocultarMostrarBusqueda() {
+        if (filtrosBusquedaAvanzada.style.display === 'none' || filtrosBusquedaAvanzada.style.display === '') {
+            filtrosBusquedaAvanzada.style.display = 'block';
+        } else {
+            filtrosBusquedaAvanzada.style.display = 'none';
+        }
     }
-}
 
-btnMostrarFiltros.onclick = ocultarMostrarBusqueda; 
+    btnMostrarFiltros.onclick = ocultarMostrarBusqueda;
+    
+});
